3ff4ba5ac4e2ad465da86fa2c0964a5db51d9e1d
Leonardo Bradorda <Theleonardovictorteodoro@gmail.com>
2026-01-17 20:47:46 -0300

    Sending new project exception-handlers-II

---
+++ Exceptions and Handlers/exception-handlers-II/src/main/java/com/bradorda/exception_handlers_II/ExceptionHandlersIiApplication.java
@@ -0,0 +1,13 @@
+package com.bradorda.exception_handlers_II;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlersIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlersIiApplication.class, args);
+	}
+
+}
---
+++ Exceptions and Handlers/exception-handlers-II/src/test/java/com/bradorda/exception_handlers_II/ExceptionHandlersIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bradorda.exception_handlers_II;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlersIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
